6f320ed62e3382cc8cb162e9dd809827c12dc1d8
vrk <victoriakirst+github@gmail.com>
2021-03-07 11:56:47 -0500

    updated with unique links

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,27 @@
-const { DateTime } = require('luxon');
-const readingTime = require('eleventy-plugin-reading-time');
-const pluginRss = require('@11ty/eleventy-plugin-rss');
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
-const fs = require('fs');
-const path = require('path');
+const { DateTime } = require("luxon");
+const readingTime = require("eleventy-plugin-reading-time");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const fs = require("fs");
+const path = require("path");
+const markdownLibrary = require("markdown-it")();
+const mdHeadings = require("markdown-it-anchor");
 
-const isDev = process.env.APP_ENV === 'development';
+const isDev = process.env.APP_ENV === "development";
 
 const manifestPath = path.resolve(
   __dirname,
-  'public',
-  'assets',
-  'manifest.json'
+  "public",
+  "assets",
+  "manifest.json"
 );
 
 const manifest = isDev
   ? {
-      'main.js': '/assets/main.js',
-      'main.css': '/assets/main.css',
+      "main.js": "/assets/main.js",
+      "main.css": "/assets/main.css",
     }
-  : JSON.parse(fs.readFileSync(manifestPath, { encoding: 'utf8' }));
+  : JSON.parse(fs.readFileSync(manifestPath, { encoding: "utf8" }));
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(readingTime);
@@ -27,37 +29,37 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
 
   eleventyConfig.setDataDeepMerge(true);
-  eleventyConfig.addPassthroughCopy({ 'src/images': 'images' });
+  eleventyConfig.addPassthroughCopy({ "src/images": "images" });
   eleventyConfig.setBrowserSyncConfig({ files: [manifestPath] });
 
-  eleventyConfig.addShortcode('bundledcss', function () {
-    return manifest['main.css']
-      ? `<link href="${manifest['main.css']}" rel="stylesheet" />`
-      : '';
+  eleventyConfig.addShortcode("bundledcss", function () {
+    return manifest["main.css"]
+      ? `<link href="${manifest["main.css"]}" rel="stylesheet" />`
+      : "";
   });
 
-  eleventyConfig.addShortcode('bundledjs', function () {
-    return manifest['main.js']
-      ? `<script src="${manifest['main.js']}"></script>`
-      : '';
+  eleventyConfig.addShortcode("bundledjs", function () {
+    return manifest["main.js"]
+      ? `<script src="${manifest["main.js"]}"></script>`
+      : "";
   });
 
-  eleventyConfig.addFilter('excerpt', (post) => {
-    const content = post.replace(/(<([^>]+)>)/gi, '');
-    return content.substr(0, content.lastIndexOf(' ', 200)) + '...';
+  eleventyConfig.addFilter("excerpt", (post) => {
+    const content = post.replace(/(<([^>]+)>)/gi, "");
+    return content.substr(0, content.lastIndexOf(" ", 200)) + "...";
   });
 
-  eleventyConfig.addFilter('readableDate', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
-      'dd LLL yyyy'
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLL yyyy"
     );
   });
 
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
   });
 
-  eleventyConfig.addFilter('head', (array, n) => {
+  eleventyConfig.addFilter("head", (array, n) => {
     if (n < 0) {
       return array.slice(n);
     }
@@ -65,18 +67,18 @@ module.exports = function (eleventyConfig) {
     return array.slice(0, n);
   });
 
-  eleventyConfig.addCollection('tagList', function (collection) {
+  eleventyConfig.addCollection("tagList", function (collection) {
     let tagSet = new Set();
     collection.getAll().forEach(function (item) {
-      if ('tags' in item.data) {
+      if ("tags" in item.data) {
         let tags = item.data.tags;
 
         tags = tags.filter(function (item) {
           switch (item) {
-            case 'all':
-            case 'nav':
-            case 'post':
-            case 'posts':
+            case "all":
+            case "nav":
+            case "post":
+            case "posts":
               return false;
           }
 
@@ -92,28 +94,31 @@ module.exports = function (eleventyConfig) {
     return [...tagSet];
   });
 
-  eleventyConfig.addFilter('pageTags', (tags) => {
-    const generalTags = ['all', 'nav', 'post', 'posts'];
+  eleventyConfig.addFilter("pageTags", (tags) => {
+    const generalTags = ["all", "nav", "post", "posts"];
 
     return tags
       .toString()
-      .split(',')
+      .split(",")
       .filter((tag) => {
         return !generalTags.includes(tag);
       });
   });
 
+  markdownLibrary.use(mdHeadings);
+  eleventyConfig.setLibrary("md", markdownLibrary);
+
   return {
     dir: {
-      input: 'src',
-      output: 'public',
-      includes: 'includes',
-      data: 'data',
-      layouts: 'layouts',
+      input: "src",
+      output: "public",
+      includes: "includes",
+      data: "data",
+      layouts: "layouts",
       passthroughFileCopy: true,
-      templateFormats: ['html', 'njk', 'md'],
-      htmlTemplateEngine: 'njk',
-      markdownTemplateEngine: 'njk',
+      templateFormats: ["html", "njk", "md"],
+      htmlTemplateEngine: "njk",
+      markdownTemplateEngine: "njk",
     },
   };
 };
